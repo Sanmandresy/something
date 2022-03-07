15a749f5e1b4ee3825bc208d860b323e11e8d719
Rojotiana Nomena <hei.nomena@gmail.com>
2022-03-07 23:52:20 +0300

    fix: Validation W3C

--- index.php
+++ index.php
@@ -7,10 +7,12 @@
 
 	<!-- Bootstrap CSS -->
 	<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous" />
-	<link href="./css/principale.css" />
+	<link href="./css/principale.css" rel="stylesheet"/>
+
+	<title>Something</title>
 
 
-	<style>
+<style>
 		.bd-placeholder-img {
 			font-size: 1.125rem;
 			text-anchor: middle;
